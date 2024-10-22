01fc687fac32931973100b8c09e081eef6b6aeb0
novalrnv <akuunoval@gmail.com>
2024-10-22 19:31:21 +0700

    p3

---
+++ triangle12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class triangle12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // System.out.print("Masukkan nilai N : ");
+        // int n = input.nextInt();
+        int n = 5;
+        int i = 0;
+        while(i <= n){
+            int j=0;
+            while(j<i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
