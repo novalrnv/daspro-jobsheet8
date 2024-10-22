47801a25adf3f7b98b2fff47fcb9b56845dacbe0
novalrnv <akuunoval@gmail.com>
2024-10-22 18:03:59 +0700

    p1

---
+++ Star12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Star12{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan milai N : ");
+        int n = input.nextInt();
+
+        for(int i=1 ; i<=n ; i--){
+            System.out.print("*");
+        }
+    }
+}
